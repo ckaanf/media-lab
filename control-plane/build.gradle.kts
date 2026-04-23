ae371cf60d66969924a61457beb2082bbd643d43
ckaanf <skywlstn777@gmail.com>
2026-04-23 17:22:12 +0900

    test용 db 의존성 주입

--- control-plane/build.gradle.kts
+++ control-plane/build.gradle.kts
@@ -27,9 +27,14 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
+
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    runtimeOnly("com.h2database:h2")
+    testRuntimeOnly("com.h2database:h2")
+
     runtimeOnly("org.postgresql:postgresql")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
