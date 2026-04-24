c6d26db5ae31794609f456d4a0932addd33a1392
ckaanf <skywlstn777@gmail.com>
2026-04-24 09:07:24 +0900

    지연 출력으로 변경

--- control-plane/src/main/java/ckaanf/controlplane/streaming/common/exception/StreamingDomainException.java
+++ control-plane/src/main/java/ckaanf/controlplane/streaming/common/exception/StreamingDomainException.java
@@ -5,9 +5,19 @@ import lombok.Getter;
 @Getter
 public class StreamingDomainException extends RuntimeException {
     private final StreamingErrorCode errorCode;
+    private final Object[] args;
 
     public StreamingDomainException(StreamingErrorCode errorCode , Object... args) {
-        super(String.format(errorCode.getMessage(), args));
+        super(errorCode.getCode());
         this.errorCode = errorCode;
+        this.args = args;
+    }
+
+    @Override
+    public String getMessage() {
+        if (args == null || args.length == 0) {
+            return errorCode.getMessage();
+        }
+        return String.format(errorCode.getMessage(), args);
     }
 }
